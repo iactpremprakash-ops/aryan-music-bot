9d328eb0589a2629b781853a99a78b3b6d29e222
iactpremprakash-ops <iactpremprakash@gmail.com>
2026-06-02 19:33:18 +0530

    Create config.py
    Configuration file for bot environment variables

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "12345"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+MONGO_URL = os.getenv("MONGO_URL", "")
